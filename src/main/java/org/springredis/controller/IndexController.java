07fdec28fa0f87246595980139a3d3db0c6882a6
kaushik <sleva.kaushik@machinepulse.com>
2018-06-19 14:05:31 +0530

    Added default api to ignore list

--- src/main/java/org/springredis/controller/IndexController.java
+++ src/main/java/org/springredis/controller/IndexController.java
@@ -2,11 +2,13 @@ package org.springredis.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import springfox.documentation.annotations.ApiIgnore;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
+    @ApiIgnore
     public String getIndexPage(){
         return "redirect:/swagger-ui.html";
     }
