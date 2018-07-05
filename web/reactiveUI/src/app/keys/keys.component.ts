7c978bfb70ec6f323a9d097715acc931eb22f2d1
Kaushik <gtskaushik@gmail.com>
2018-07-05 09:27:02 +0530

    Completed UI

--- web/reactiveUI/src/app/keys/keys.component.ts
+++ web/reactiveUI/src/app/keys/keys.component.ts
@@ -1,5 +1,5 @@
 import { Input, Output, EventEmitter, Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-keys',
